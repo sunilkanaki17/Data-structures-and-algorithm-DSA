bd76ce3f1d539bfadf9cdc6323a7848da01db973
Sunil Kanaki <sunilkanaki17@gmail.com>
2024-07-17 07:43:44 +0530

    Update binary_search_tree.cpp

--- Trees/binary_search_tree.cpp
+++ Trees/binary_search_tree.cpp
@@ -103,10 +103,85 @@ void levelorder(Node *root)
 
 }
 
+bool search(Node *root,int key)
+{
+    if(root == NULL)
+    {
+        return false;
+    }
+    if(key==root->data)
+    {
+        return true;
+    }
+    else if(key > root->data)
+    {
+        return search(root->right,key);
+    }
+    else if(key < root->data)
+    {
+        return search(root->left,key);
+    }
+}
+
+Node *deletefromBST(Node *root,int val)
+{
+    if(root==NULL)
+        return root;
+    if(root->data == val)
+    {
+
+        //0 child
+        if(root->left==NULL && root->right==NULL)
+        {
+            free(root);
+            return NULL;
+        }
+
+        //1 child
+        //left node
+        if(root->left!=NULL && root->right==NULL)
+        {
+            Node *temp = root->left;
+            free(root);
+            return temp;
+
+        }
+        if(root->left==NULL && root->right!=NULL){
+            Node *temp = root->right;
+            free(root);
+            return temp;
+        }
+
+        //2 child
+        if(root->left!=NULL && root->right!=NULL)
+        {
+            Node *temp = root->right;
+            while(temp->left!=NULL)
+            {
+                temp = temp->left;
+            }
+            int mini = temp->data;
+            root->data = mini;
+            root->right = deletefromBST(root->right,mini);
+            return root;
+        }
+    }
+    else if(val > root->data)
+    {
+        root->right = deletefromBST(root->right,val);
+        return root;
+    }
+    else if(val  < root->data)
+    {
+        root->left = deletefromBST(root->left,val);
+        return root;
+    }
+}
+
 int main()
 {
     Node *root = NULL;
-    int d;
+    int d,key,val;
     cout<<"Enter the data (-1 to exit)"<<endl;
     cin>>d;
     while(d!=-1)
@@ -130,4 +205,31 @@ int main()
     levelorder(root);
     cout<<endl;
 
+    cout<<"Enter the data to be searched"<<endl;
+    cin>>key;
+    if(search(root,key))
+        cout<<"Data found"<<endl;
+    else    
+        cout<<"No data found"<<endl;
+
+    cout<<"Enter the data to be deleted"<<endl;
+    cin>>val;
+    root = deletefromBST(root,val);
+
+    cout<<"Inorder : ";
+    inorder(root);
+    cout<<endl;
+
+    cout<<"Preorder : ";
+    preorder(root);
+    cout<<endl;
+
+    cout<<"Postorder : ";
+    postorder(root);
+    cout<<endl;
+
+    cout<<"Levelorder : "<<endl;
+    levelorder(root);
+    cout<<endl;
+    
 }
